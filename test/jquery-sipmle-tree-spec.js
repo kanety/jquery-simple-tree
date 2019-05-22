2279040adfeb5945455f10e0f17ab3200659a08c
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2019-05-22 13:20:59 +0900

    Fix test

--- test/jquery-sipmle-tree-spec.js
+++ test/jquery-sipmle-tree-spec.js
@@ -1,12 +1,11 @@
 describe('jquery-simple-tree', function() {
   beforeEach(function() {
     document.body.innerHTML = __html__['index.html'];
+    eval($('script').text());
   });
 
   it('opens or closes a node', function() {
     var $tree = $('#basic');
-    $tree.simpleTree();
-
     var $node = $tree.find('li[data-node-id="1.1"]');
     var $icon = $node.children('.tree-icon');
 
@@ -20,10 +19,6 @@ describe('jquery-simple-tree', function() {
     var $tree = $('#basic');
     var $expander = $('#expander');
     var $collapser = $('#collapser');
-    $tree.simpleTree({
-      expander: $expander,
-      collapser: $collapser
-    });
 
     $collapser.click();
     expect($tree.find('ul:visible').length).toEqual(0);
@@ -33,9 +28,6 @@ describe('jquery-simple-tree', function() {
 
   it('specifies opened nodes', function() {
     var $tree = $('#opened');
-    $tree.simpleTree({
-      opened: [1, 1.1]
-    });
 
     expect($tree.find('li[data-node-id="1"]').is(':visible')).toEqual(true);
     expect($tree.find('li[data-node-id="1.1"]').is(':visible')).toEqual(true);
@@ -46,15 +38,9 @@ describe('jquery-simple-tree', function() {
   it('has callbacks', function() {
     var $tree = $('#callback');
     var $message = $('#message');
-    $tree.simpleTree().on('node:open', function(e, $node) {
-      $message.append("opened " + $node.data('node-id') + " ");
-    }).on('node:close', function(e, $node) {
-      $message.append("closed " + $node.data('node-id') + " ");
-    });
 
     $tree.find('li[data-node-id="1.1"] .tree-icon').click().click();
     expect($message.html()).toContain("opened 1.1");
     expect($message.html()).toContain("closed 1.1");
   });
-
 });
